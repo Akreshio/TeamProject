1130665f2d6e7c2f8259a99854259a0c41b4e7f0
GarryProhor <84273694+GarryProhor@users.noreply.github.com>
2022-03-20 13:52:35 +0300

    ApplicationConfig

---
+++ src/main/java/ru/intervale/TeamProject/config/ApplicationConfig.java
@@ -0,0 +1,20 @@
+package ru.intervale.TeamProject.config;
+
+import lombok.Data;
+import lombok.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "teamproject")
+public class ApplicationConfig {
+
+
+
+    private String name;
+
+    private String email;
+
+    private String version;
+}
--- src/main/java/ru/intervale/TeamProject/config/OpenApiConfig.java
+++ src/main/java/ru/intervale/TeamProject/config/OpenApiConfig.java
@@ -6,11 +6,16 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class OpenApiConfig {
+
+    private final ApplicationConfig config;
+
     @Bean
     public OpenAPI customOpenApi(){
         return new OpenAPI()
@@ -18,12 +23,8 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("Dinamic change price Book API")
                         .description("This is TeamTask Intervale Company")
-                        .contact(new Contact().email("group-B@teamproject.ru"))
-                        .license(new License().name("group-B: " +
-                                                    "Дробышевский В., " +
-                                                    "Прохорченко И., " +
-                                                    "Маевский С., " +
-                                                    "Самусев Д"))
-                        .version("1.0"));
+                        .contact(new Contact().email(config.getEmail()))
+                        .license(new License().name(config.getName()))
+                        .version(config.getVersion()));
     }
 }
