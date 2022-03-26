a3bebcbd46a46b79fb8276394ac7567d3ac1ac8c
Гарри Прохор <84273694+GarryProhor@users.noreply.github.com>
2022-03-26 15:57:31 +0300

    other fix

--- src/main/java/ru/intervale/TeamProject/controller/BookPriceController.java
+++ src/main/java/ru/intervale/TeamProject/controller/BookPriceController.java
@@ -11,8 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.intervale.TeamProject.model.book.BookEntity;
 import ru.intervale.TeamProject.model.request.ParamRequest;
 import ru.intervale.TeamProject.model.request.Period;
-import ru.intervale.TeamProject.service.rate.Currency;
 import ru.intervale.TeamProject.service.PriceDynamicService;
+import ru.intervale.TeamProject.service.rate.Currency;
+
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -32,7 +33,7 @@ public class BookPriceController implements BookPrice {
     private static final String IMAGE_SVG = "image/svg";
 
     @Override
-    public ResponseEntity<byte[]> getPdf(String name, Currency currency, String sStr,String fStr, Period d){
+    public ResponseEntity<byte[]> getPdf(String name, Currency currency, String sStr, String fStr, Period d){
 
         ParamRequest param = null;
         if ((sStr!=null)||(fStr!=null)||(d!=null)){
--- src/main/java/ru/intervale/TeamProject/service/generator/impl/PdfGeneratorServiceImpl.java
+++ src/main/java/ru/intervale/TeamProject/service/generator/impl/PdfGeneratorServiceImpl.java
@@ -9,6 +9,7 @@ package ru.intervale.TeamProject.service.generator.impl;
 
 import java.io.ByteArrayOutputStream;
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.time.LocalDateTime;
 
 import java.time.format.DateTimeFormatter;
@@ -33,6 +34,7 @@ import org.springframework.stereotype.Service;
 
 import ru.intervale.TeamProject.model.book.BookEntity;
 import ru.intervale.TeamProject.service.generator.PdfGeneratorService;
+
 
 @Slf4j
 @Service
@@ -65,17 +67,34 @@ public class PdfGeneratorServiceImpl implements PdfGeneratorService {
 
     private void header(Document document, BookEntity book) throws DocumentException {
         //создание заголовка над таблицей с названием книги
-        Font font = FontFactory.getFont(FontFactory.COURIER, 14, BaseColor.BLACK);
-        Paragraph paragraph = new Paragraph(" Book " + book.getTitle(), font);
+        Font font = FontFactory.getFont(FontFactory.TIMES_ROMAN, 14, BaseColor.BLACK);
+        Paragraph paragraph = new Paragraph(book.getTitle(), font);
         paragraph.setAlignment(Element.ALIGN_CENTER);
 
+        Paragraph paragraph1 = new Paragraph(" page: " + book.getPage(), font);
+        paragraph1.setAlignment(Element.ALIGN_LEFT);
+        Paragraph paragraph2 = new Paragraph(" isbn: " + book.getIsbn(), font);
+        paragraph2.setAlignment(Element.ALIGN_LEFT);
+        Paragraph paragraph3 = new Paragraph(" price: " + book.getPrice(), font);
+        paragraph3.setAlignment(Element.ALIGN_LEFT);
+        Paragraph paragraph4 = new Paragraph(" weight: " + book.getWeight(), font);
+        paragraph4.setAlignment(Element.ALIGN_LEFT);
+
+
+
         document.add(paragraph);
+        document.add(paragraph1);
+        document.add(paragraph2);
+        document.add(paragraph3);
+        document.add(paragraph4);
         document.add(Chunk.NEWLINE);
     }
 
     private void body(Document document, BookEntity book) throws DocumentException {
+
         // создание таблицы ->
         PdfPTable table = new PdfPTable(2);
+
         // добавление шапки таблицы  ->
         Stream.of("Date", "Price in currency")
                 .forEach(headerTitle -> {
@@ -87,22 +106,31 @@ public class PdfGeneratorServiceImpl implements PdfGeneratorService {
                     header.setPhrase(new Phrase(headerTitle, headFont));
                     table.addCell(header);
                 });
+
         // вывод значений изменения цены
         if ((book.getChangePrice() != null) && (!book.getChangePrice().isEmpty())){
 
             for (Map.Entry<LocalDateTime, BigDecimal> price : book.getChangePrice().entrySet()) {
                 createCell(table, formatDate(price.getKey()));
-                createCell(table, String.valueOf(price.getValue()));
+                createCell(table, formatPrice(price.getValue()));
             }
             document.add(table);
         }
     }
 
+    //формат времени 01.01.2021 00:00
     private String formatDate (LocalDateTime date){
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
         return  formatter.format(date);
     }
 
+    //формат цены 99.99
+    private String formatPrice(BigDecimal price) {
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+        return decimalFormat.format(price);
+    }
+
+    //создание ячеек таблицы
     private void createCell (PdfPTable table, String str){
         PdfPCell cell = new PdfPCell(new Phrase(str));
         cell.setPaddingLeft(4);
