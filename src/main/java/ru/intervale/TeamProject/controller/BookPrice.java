8e16b9fb02b078882be32e74cdeaca0a1f0cf53a
Гарри Прохор <84273694+GarryProhor@users.noreply.github.com>
2022-03-27 21:26:27 +0300

    fixes BookPrice

--- src/main/java/ru/intervale/TeamProject/controller/BookPrice.java
+++ src/main/java/ru/intervale/TeamProject/controller/BookPrice.java
@@ -17,102 +17,100 @@ import ru.intervale.TeamProject.service.rate.Currency;
 
 import java.util.List;
 
-import java.io.IOException;
-
 public interface BookPrice {
 
 
-    @ApiOperation(value = "Получение изменения цены на книгу", nickname = "getJson", notes = "Возвращает запрошенный формат", tags={"public"})
+    @ApiOperation(value = "Getting a price change for a book", nickname = "getJson", notes = "Returns the requested format", tags={"public"})
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Успешное выполнение запроса", response = Response.class),
-            @ApiResponse(code = 400, message = "Некорректный запрос", response = Error.class),
-            @ApiResponse(code = 500, message = "Ошибка выполнения запроса", response = Error.class)
+            @ApiResponse(code = 200, message = "Successful execution of the request", response = Response.class),
+            @ApiResponse(code = 400, message = "Bad request", response = Error.class),
+            @ApiResponse(code = 500, message = "Request execution error", response = Error.class)
     })
     @RequestMapping(value = "/1.0.0/price/stat",
             produces = { "application/json;charset=UTF-8"},
             method = RequestMethod.GET)
     ResponseEntity<List<BookEntity>> getJson(
-            @ApiParam(value = "Наименование книги")
+            @ApiParam(value = "Book title")
             @RequestParam(value = "name") String name,
-            @ApiParam(value = "код валюты")
+            @ApiParam(value = "currency code")
             @RequestParam(value = "currency") Currency currency,
 
 
             @Pattern(value = "^(3[01]|[12][0-9]|0[1-9]).(1[0-2]|0[1-9]).((20)[0-9]{2})$")
-            @RequestParam(value = "s", required=false) String sStr,
+            @RequestParam(value = "beginning of period (01.01.2022)", required=false) String sStr,
             @Pattern(value = "^(3[01]|[12][0-9]|0[1-9]).(1[0-2]|0[1-9]).((20)[0-9]{2})$")
-            @RequestParam(value = "f", required=false) String fStr,
+            @RequestParam(value = "end of period (01.01.2022)", required=false) String fStr,
 
-            @RequestParam(value = "d", required=false) Period d
+            @RequestParam(value = "period", required=false) Period d
     );
 
-    @ApiOperation(value = "Получение изменения цены на книгу", nickname = "getPdf", notes = "Возвращает запрошенный формат", tags={"public"})
+    @ApiOperation(value = "Getting a price change for a book", nickname = "getPdf", notes = "Returns the requested format", tags={"public"})
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Успешное выполнение запроса", response = Response.class),
-            @ApiResponse(code = 400, message = "Некорректный запрос", response = Error.class),
-            @ApiResponse(code = 500, message = "Ошибка выполнения запроса", response = Error.class)
+            @ApiResponse(code = 200, message = "Successful execution of the request", response = Response.class),
+            @ApiResponse(code = 400, message = "Bad request", response = Error.class),
+            @ApiResponse(code = 500, message = "Request execution error", response = Error.class)
     })
     @RequestMapping(value = "/1.0.0/price/stat",
             produces = { "application/pdf"},
             method = RequestMethod.GET)
     ResponseEntity<byte[]> getPdf(
-            @ApiParam(value = "Наименование книги")
+            @ApiParam(value = "Book title")
             @RequestParam(value = "name") String name,
-            @ApiParam(value = "код валюты")
+            @ApiParam(value = "currency code")
             @RequestParam(value = "currency") Currency currency,
 
             @Pattern(value = "^(3[01]|[12][0-9]|0[1-9]).(1[0-2]|0[1-9]).((20)[0-9]{2})$")
-            @RequestParam(value = "s", required=false) String sStr,
+            @RequestParam(value = "beginning of period (01.01.2022)", required=false) String sStr,
             @Pattern(value = "^(3[01]|[12][0-9]|0[1-9]).(1[0-2]|0[1-9]).((20)[0-9]{2})$")
-            @RequestParam(value = "f", required=false) String fStr,
+            @RequestParam(value = "end of period (01.01.2022)", required=false) String fStr,
 
-            @RequestParam(value = "d", required=false) Period d
+            @RequestParam(value = "period", required=false) Period d
     );
 
-    @ApiOperation(value = "Получение изменения цены на книгу", nickname = "getSvg", notes = "Возвращает запрошенный формат", tags={"public"})
+    @ApiOperation(value = "Getting a price change for a book", nickname = "getSvg", notes = "Returns the requested format", tags={"public"})
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Успешное выполнение запроса", response = Response.class),
-            @ApiResponse(code = 400, message = "Некорректный запрос", response = Error.class),
-            @ApiResponse(code = 500, message = "Ошибка выполнения запроса", response = Error.class)
+            @ApiResponse(code = 200, message = "Successful execution of the request", response = Response.class),
+            @ApiResponse(code = 400, message = "Bad request", response = Error.class),
+            @ApiResponse(code = 500, message = "Request execution error", response = Error.class)
     })
     @RequestMapping(value = "/1.0.0/price/stat",
             produces = { "image/svg+xml"},
             method = RequestMethod.GET)
     ResponseEntity<byte[]> getSvg(
-            @ApiParam(value = "Наименование книги")
+            @ApiParam(value = "Book title")
             @RequestParam(value = "name") String name,
-            @ApiParam(value = "код валюты")
+            @ApiParam(value = "currency code")
             @RequestParam(value = "currency") Currency currency,
 
             @Pattern(value = "^(3[01]|[12][0-9]|0[1-9]).(1[0-2]|0[1-9]).((20)[0-9]{2})$")
-            @RequestParam(value = "s", required=false) String sStr,
+            @RequestParam(value = "beginning of period (01.01.2022)", required=false) String sStr,
             @Pattern(value = "^(3[01]|[12][0-9]|0[1-9]).(1[0-2]|0[1-9]).((20)[0-9]{2})$")
-            @RequestParam(value = "f", required=false) String fStr,
+            @RequestParam(value = "end of period (01.01.2022)", required=false) String fStr,
 
-            @RequestParam(value = "d", required=false) Period d
-    ) throws IOException;
+            @RequestParam(value = "period", required=false) Period d
+    );
 
-    @ApiOperation(value = "Получение изменения цены на книгу", nickname = "getCsv", notes = "Возвращает запрошенный формат", tags={"public"})
+    @ApiOperation(value = "Getting a price change for a book", nickname = "getCsv", notes = "Returns the requested format", tags={"public"})
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Успешное выполнение запроса", response = Response.class),
-            @ApiResponse(code = 400, message = "Некорректный запрос", response = Error.class),
-            @ApiResponse(code = 500, message = "Ошибка выполнения запроса", response = Error.class)
+            @ApiResponse(code = 200, message = "Successful execution of the request", response = Response.class),
+            @ApiResponse(code = 400, message = "Bad request", response = Error.class),
+            @ApiResponse(code = 500, message = "Request execution error", response = Error.class)
     })
     @RequestMapping(value = "/1.0.0/price/stat",
             produces = { "text/csv;charset=UTF-8"},
             method = RequestMethod.GET)
     ResponseEntity<String> getCsv(
-            @ApiParam(value = "Наименование книги")
+            @ApiParam(value = "Book title")
             @RequestParam(value = "name") String name,
-            @ApiParam(value = "код валюты")
+            @ApiParam(value = "currency code")
             @RequestParam(value = "currency") Currency currency,
 
             @Pattern(value = "^(3[01]|[12][0-9]|0[1-9]).(1[0-2]|0[1-9]).((20)[0-9]{2})$")
-            @RequestParam(value = "s", required=false) String sStr,
+            @RequestParam(value = "beginning of period (01.01.2022)", required=false) String sStr,
             @Pattern(value = "^(3[01]|[12][0-9]|0[1-9]).(1[0-2]|0[1-9]).((20)[0-9]{2})$")
-            @RequestParam(value = "f", required=false) String fStr,
+            @RequestParam(value = "end of period (01.01.2022)", required=false) String fStr,
 
-            @RequestParam(value = "d", required=false) Period d
+            @RequestParam(value = "period", required=false) Period d
     );
 
 
